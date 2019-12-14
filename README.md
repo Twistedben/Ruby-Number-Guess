f466d43c26508fffd57684aa1981d687b2f13a53
Benjamin Broestl <Bensgamingmail@yahoo.com>
2019-12-14 15:39:09 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Ruby Guessing Game
+
+## Description
+
+A simple command line game where you enter your name, and then have three attempts to guess a number from 0 to 10. Wrote it just to practice some Ruby input/output fundamentals.
+
+### Notes
+
+To run, ensure Ruby is installed on your OS. CD to location and run `ruby number_guessing_game.rb`
---
+++ number_guessing_game.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby 
+
+
+puts "==========================="
+puts "| Ruby Number Guess Game |"
+puts "==========================="
+puts "==========================="
+print "What is your name? "
+
+
+response = gets.chomp
+
+puts "Hello, #{response}! "
+puts "Guess a number from 0 to 10. You have three guesses."
+
+random_number_answer = rand(11)
+
+3.times do |i| 
+  puts "Guess #{i + 1}:" 
+  guessed_answer = gets.chomp
+  guessed_answer = guessed_answer.to_i
+
+  if guessed_answer > 10 || guessed_answer < 0
+    puts "Your number has to be between 0 and 10"
+  else
+    if guessed_answer == random_number_answer
+      abort("Correct! Good job!")
+    else
+      puts "Try again!" unless i == 2
+      puts "Sorry, you're out of guesses. The number was: #{random_number_answer}" if i == 2
+    end
+  end
+
+end
